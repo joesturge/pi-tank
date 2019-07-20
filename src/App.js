a051587da0df71cb11650ed0d6204f5a3e7e2086
joesturge <joe.mike.sturge@gmail.com>
2019-07-20 01:55:21 +0100

    finished front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Div100vh from 'react-div-100vh';
+import JoyStick from 'react-joystick';
+import axios from "axios";
+
+import 'rc-slider/assets/index.css';
+
+const joyOptions = {
+    mode: 'dynamic',
+    catchDistance: 150,
+    color: 'white',
+    lockY: true
+}
+
+const handleInput = (name, value) => {
+  switch(name) {
+    case "LEFT":
+    case "RIGHT":
+      axios.post("/control", {
+        value,
+        name
+      });
+      break;
+    default:
+  }
+};
+
+const left = manager => managerListener(manager, "LEFT");
+const right = manager => managerListener(manager, "RIGHT");
+
+const managerListener = (manager, name) => {
+  manager.on('move', (e, stick) => {
+    handleInput(name, Math.round(255 * (stick.instance.position.y - stick.position.y) / 50));
+  });
+  manager.on('end', () => {
+    handleInput(name, 0);
+  });
+};
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div100vh>
+    <div style={{backgroundColor: "#282c34", display: "flex", height: "100%", width: "100%", justifyContent: "space-between", alignItems: "center"}}>
+        <div style={{height: "100%", width: "50%"}}>
+          <JoyStick options={joyOptions} containerStyle={{position: "relative", height: "100%", width: "100%"}} managerListener={left} />
+        </div>
+        <div style={{backgroundColor: "#682c34", height: "100%", width: "50%"}}>
+          <JoyStick options={joyOptions} containerStyle={{position: "relative", height: "100%", width: "100%"}} managerListener={right} />
+        </div>
+      </div>
+    </Div100vh>
   );
 }
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,11 @@
+const bodyParser = require('body-parser');
+
 module.exports = app => {
-  app.post("control", (request, response) => {
+
+  app.use(bodyParser.json());
+
+  app.post("/control", (request, response) => {
     console.log(request.body);
     response.status(200).end();
   });
-}
+};
